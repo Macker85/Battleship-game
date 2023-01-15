c73f05771162a17220ecd7dffd4a127b59ac84f7
Macker85 <pmacker1985@gmail.com>
2023-01-15 21:09:28 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class GameBoard:
+    def__init__(self, board):
+        self.board = board
+
+    def get_letters_to_numbers():
+        letter_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6}
+        return letter_to_numbers
+
+    def print_game(self):
+        print(" A B C D E F G")
+        print(" -------------")
+        row_number = 1
+        for row in self.board:
+            print("%d|%s|" % (row_number, "|".join(row)))
+
