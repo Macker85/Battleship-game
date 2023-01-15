98ba5026bab62dccd31e2dd123f53197e631ede3
Macker85 <pmacker1985@gmail.com>
2023-01-15 21:31:51 +0000

    ADD:board function for game

--- run.py
+++ run.py
@@ -1,18 +1,41 @@
 import random
 
-class GameBoard:
-    def__init__(self, board):
-        self.board = board
+# class GameBoard:
+#     def__init__(self, board):
+#         self.board = board
 
-    def get_letters_to_numbers():
-        letter_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6}
-        return letter_to_numbers
+    # def get_letters_to_numbers():
+    #     letter_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6}
+    #     return letter_to_numbers
 
-    def print_game(self):
-        print(" A B C D E F G")
-        print(" -------------")
-        row_number = 1
-        for row in self.board:
-            print("%d|%s|" % (row_number, "|".join(row)))
-            row_number +=1
+    # def print_game(self):
+    #     print(" A B C D E F G")
+    #     print(" -------------")
+    #     row_number = 1
+    #     for row in self.board:
+    #         print("%d|%s|" % (row_number, "|".join(row)))
+    #         row_number +=1
+
+def create_board(board):
+    print(" A B C D E F G")
+    print(" -------------")
+    row_num = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_num += 1
+
+def make_ships():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_player_hits():
+    pass
+
+create_ships()
+turns = 10
+while turns > 0
+
+
 
